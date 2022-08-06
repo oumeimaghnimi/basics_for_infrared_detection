a8dac88526f3c92c608e99d6d34c009179e47fe1
oumeimaghnimi <84542290+oumeimaghnimi@users.noreply.github.com>
2022-08-06 19:48:39 +0000

    Update rename_files_matlab.m

--- dataset/rename_files_matlab.m
+++ dataset/rename_files_matlab.m
@@ -1,25 +1,12 @@
 
-%first step: add all folders and subfolders and scripts to path by double
-%clic and choose add to path
-
-for j =1: 4
-    %folder_ann = uigetdir('H:\video_to_apply_SSL\LSOTB-TIR_TrainingData\test_sur_four_lstob\','Select Input-folder-annotations'); 
-    %folder_ann = uigetdir('D:\Matlab C\TLBO\test_sur_four_lstob' ,'Select Input-folder-annotations');
-    folder_ann = uigetdir('\content\gdrive\MyDrive\Colab Notebooks\first trial\basics_for_infrared_detection\dataset\LSOTB-TIR_TrainingData\Annotations\' ,'Select Input-folder');
-    
-   %choice between TIR_training_001, TIR_training_002, TIR_training_003,etc.
-    %folder_ann= H:\video_to_apply_SSL\LSOTB-TIR_TrainingData\Annotations\test_sur_four_lstob\TIR_training_001
-         % in workspace (type: string)
     folder_ann = '/content/gdrive/MyDrive/Colab Notebooks/first trial/basics_for_infrared_detection/dataset/LSOTB-TIR_TrainingData_part5/TIR_training_004/TIR_training_004_ann/'
-    G_ann = dir(folder_ann);  %dir for folder_ann type string
+    G_ann = dir(folder_ann);  %G_ann: string
     G_ann = G([G.isdir]); 
     G_ann = G_ann(~ismember({G_ann.name}, {'.' '..'}));          % out: G_ann.name :string      
-
-
     G_ann(strncmp({G_ann.name}, '.',1)) = [];  % Remove . and ..
-
     G_ann= rmfield(G_ann,{'date','bytes', 'isdir', 'datenum'}); %G_ann  Matlab  struct of string
-% G_ann=Airplaine_002, bear_001, etc
+    % G_ann=Airplaine_002, bear_001, etc
+    
     for k = 1:length(G_ann)
         %subfolder_ann = uigetdir('folder_ann','Select Input-subfolder-annotations'); 
         f_ann = fullfile(folder_ann, G_ann(k).name); %subfolder_ann=f_ann    %f_ann string path to airplane for instance
@@ -32,5 +19,6 @@ for j =1: 4
         %   mkdir mycurrentfiles  
         %attention here,after rename xml file, remove these lines:
         for h = 1:length(F_ann)
-            movefile(sprintf('%s',f_ann ,'\',F_ann(h).name),sprintf('%s',f_ann ,'\', sprintf('part_5_train_%d_',j),G_ann(k).name,'_', F_ann(h).name) )
+            movefile(sprintf('%s',f_ann ,'\',F_ann(h).name),sprintf('%s',f_ann ,'\', sprintf('part_5_train_%d_',4),G_ann(k).name,'_', F_ann(h).name) )
         end
+   end      
