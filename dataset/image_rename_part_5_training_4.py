f8e3e0eb7ee4c298970a052b4c90d34be88c2d2d
oumeimaghnimi <84542290+oumeimaghnimi@users.noreply.github.com>
2022-08-06 17:33:27 +0000

    Create image_rename_part_5_training_4.py

---
+++ dataset/image_rename_part_5_training_4.py
@@ -0,0 +1,28 @@
+import os
+import glob
+from glob import glob
+import string
+
+#for j =1: 4
+     #Directory_image = uigetdir('\content\gdrive\MyDrive\Colab Notebooks\first trial\basics_for_infrared_detection\dataset\LSOTB-TIR_TrainingData\TrainingData\' ,'Select Input-Directory');
+
+#Directory_image= '/content/gdrive/MyDrive/Colab Notebooks/first trial/basics_for_infrared_detection/dataset/LSOTB-TIR_TrainingData_part5/TIR_training_004_images\'
+Directory_image = cwd
+
+for i, folder in enumerate(os.listdir(Directory_image)):
+    
+    for count, filename in enumerate(glob(f'{folder}/*.jpg')):
+       #filename =horse_005/00000288.jpg
+     
+        dst_part1 = f"part_{5}_train_{4}_{folder}_"
+            
+        dst_part2= f"{filename}".split('/')[-1]
+        dst= f"{dst_part1}{dst_part2}"
+        dst=f"{Directory_image}/{folder}/{dst}"
+        filename = f"{Directory_image}/{filename}"
+            #dst= glob(f"{Directory}/{folder }/{dst}")
+        os.rename(filename, dst)
+
+           
+            
+
